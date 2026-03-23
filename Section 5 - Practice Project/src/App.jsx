4c61f4e691b51f0895c139ce0e3d897d25c14301
SebastianS02 <stecsebastian2002@gmail.com>
2026-03-23 09:19:26 -0400

    Completed Section 5 - Investment Calculator Practice Project

--- Section 5 - Practice Project/src/App.jsx
+++ Section 5 - Practice Project/src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+import Results from "./components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+      initialInvestment: 10000,
+      annualInvestment: 1200,
+      expectedReturn: 6,
+      duration: 10
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleChange(newValue, inputIdentifier) {
+      setUserInput(prevUserInput => {
+          return {
+              ...prevUserInput,
+              [inputIdentifier]: +newValue   //The + forces a conversion from string (event.value.target) to a number
+          };
+      });
+  }
+  
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header/>
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!isInputValid && <p className="center">Please enter a duration greater than 0.</p>}
+      {isInputValid && <Results input={userInput} />}
+    </>
   )
 }
 
